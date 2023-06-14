0e7b65376f2cb731745cd573b27af9edb0dfb80b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-14 17:47:37 +0200

    added like counter to jumbotron

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -2,11 +2,28 @@
     <div class="jumbotron">
         <img class="jumbotron-image" src="../../public/tokyo.jpg" alt="Tokyo Landscape">
         <h2>Visit Tokyo!</h2>
+        <div class="likes">
+            <button class="like-button" @click="likeIncrement()">👍</button>
+            <div class="like-count">
+                <h3>Likes: {{ likes }}</h3>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    
+    export default {
+        data() {
+            return {
+                likes: 0
+            }
+        },
+        methods: {
+            likeIncrement() {
+                this.likes++
+            }
+        }
+    }
 </script>
 
 <style scoped>
@@ -31,5 +48,24 @@
         padding: 1rem;
         background-color: rgba(0, 0, 0, 0.5);
         border-radius: 20px;
+    }
+
+    div.jumbotron > div.likes {
+        position: absolute;
+        right: 1%;
+        bottom: 1%;
+        width: 10%;
+    }
+
+    div.jumbotron > div.likes > div.like-count,
+    div.jumbotron > div.likes > button.like-button {
+        background-color: rgba(0, 0, 0, 0.6);
+        border-radius: 20px;
+        margin: .5rem;
+        padding: .3rem 1rem;
+    }
+
+    div.jumbotron > div.likes > button.like-button {
+        font-size: 2rem;
     }
 </style>
